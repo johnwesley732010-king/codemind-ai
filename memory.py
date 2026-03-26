0324c4649ed3b8cfe407d99ddcde3e93f77ee230
johnwesley732010-king <johnwesley732010@gmail.com>
2026-03-26 16:26:16 +0530

    Delete memory.py

--- memory.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-def save_memory(user,msg):
-    c=sqlite3.connect("memory.db")
-    c.execute("CREATE TABLE IF NOT EXISTS memory(user TEXT,msg TEXT)")
-    c.execute("INSERT INTO memory VALUES(?,?)",(user,msg))
-    c.commit()
-    c.close()
